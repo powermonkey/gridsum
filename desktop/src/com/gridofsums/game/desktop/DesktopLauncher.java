3afc8bfbedc4806561ee79d4ac0cb3cec3d63136
Rodolfo Cam II <rodiicam@gmail.com>
2017-07-31 06:05:24 +0800

    Initial push to master.

---
+++ desktop/src/com/gridofsums/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.gridofsums.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.gridofsums.game.GridOfSums;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new GridOfSums(), config);
+	}
+}
